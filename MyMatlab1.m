e7ae201dbaa63ed320500c3ceccc09d6e8ffe5de
gersusa <43012424+gersusa@users.noreply.github.com>
2019-09-11 21:17:41 -0400

    Add files via upload

--- MyMatlab1.m
+++ MyMatlab1.m
@@ -1,4 +1,5 @@
-function MyMatlab1(InFile1, InFile2, InFile3, InFile4, TimeLimitInSeconds, ScoringMethod, NetworkModel)
+function MyMatlab1(InFile1, InFile2, InFile3, InFile4,...
+    TimeLimitInSeconds, ScoringMethod, NetworkModel)
 %MYMATLAB1 Version1 Script. This script generates solution1.txt
 %
 % Input Arguments:
@@ -10,31 +11,17 @@ function MyMatlab1(InFile1, InFile2, InFile3, InFile4, TimeLimitInSeconds, Scori
 % InFile4: ROP file
 
 
-start=tic;
-
-  
-p_time=0;  %present_time :  p_time
-r_time=TimeLimitInSeconds;   %remaining_time: r_time
-
-
-
 fprintf('Started\n')
 % if isempty(gcp('nocreate'))
 % parpool('local',4);
 % end
-
+startTime = tic;
 [mpc,contingencies] = convert2mpc(InFile3,InFile4,InFile2,InFile1);
 %TimeLimitInSeconds=600;
 %ScoringMethod=2;
 
-p_time=p_time+toc(start); %Update the present time
-r_time=TimeLimitInSeconds-p_time;
-
-[mpcOPF, ~, mpcOPF_or] = solveSCOPF(mpc,contingencies,true,TimeLimitInSeconds, ScoringMethod, tic, p_time, r_time);
+[mpcOPF, ~, mpcOPF_or] = solveSCOPF(mpc,contingencies,...
+    true,TimeLimitInSeconds, ScoringMethod, startTime);
 save('mpc.mat','mpcOPF','mpcOPF_or','contingencies');
 create_solution1(fixGen2Normal(gen2shunts(mpcOPF)));
 end
-
-
-
-
